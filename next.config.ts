5ff0d5256e963a0df44b8dfc013e9027cd4994ca
Avinash Maurya <avmaurya07@gmail.com>
2025-08-21 20:51:21 +0530

    fdsf

--- next.config.ts
+++ next.config.ts
@@ -6,16 +6,6 @@ const nextConfig: NextConfig = {
     esmExternals: true,
   },
   transpilePackages: ["@mui/material", "@emotion/react", "@emotion/styled"],
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          "http://ec2-54-87-136-240.compute-1.amazonaws.com:3000/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
