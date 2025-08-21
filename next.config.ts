172dc77f097327bf70006704e9a32bee295eccbe
Avinash Maurya <avmaurya07@gmail.com>
2025-08-21 19:17:17 +0530

    yui

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   experimental: {
     esmExternals: true,
   },
   transpilePackages: ["@mui/material", "@emotion/react", "@emotion/styled"],
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/rooms/:path*/stream",
+        destination:
+          "http://ec2-54-87-136-240.compute-1.amazonaws.com:3000/api/rooms/:path*/stream",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
